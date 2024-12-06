3b2e977d233bca805c7ad661dd9c63654af80665
Jacquelimemunoz <munozjac3508@gmail.com>
2024-12-06 12:34:10 -0600

    Update main.js
    update

--- primera-entrega/script/main.js
+++ primera-entrega/script/main.js
@@ -3,3 +3,14 @@ alert ("hola," + nombre + " ");
 
 let edad = prompt("Cuantos anos tines?");
 alert ("Tines," + edad + " anos");
+
+for (let i = 1; i <= 5; i++) {
+    console.log(`Número: ${i}`);
+
+}
+
+let count = 1;
+while (count <= 5) {
+    console.log(`Número: ${count}`);
+    count++;
+}
